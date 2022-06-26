acf1a18c51c69e81e5a13e159c854bff066a83a8
CUBEDAO <108047629+CubeDAOFinance@users.noreply.github.com>
2022-06-26 22:54:53 +0600

    cap -> cube

--- contracts/interfaces/ICubeswapV2Factory.sol
+++ contracts/interfaces/ICubeswapV2Factory.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0;
 
-interface ICapswapV2Factory {
+interface ICubeswapV2Factory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
     function initCodeHash() external view returns (bytes32);
